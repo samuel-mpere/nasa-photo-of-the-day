2d5b2471852cccc74ad72f42a6834617096dd16d
chudnofskyATD <mpereannor@gmail.com>
2019-09-11 21:30:12 +0000

    css styling

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -5,7 +5,7 @@ function Details (props) {
 
     return (
         <div className ='infoNasa'>
-            <h1>{title}</h1>;
+            <h1>{title}</h1>
             <p>{date}</p>
             <p>{explanation}</p>
         </div>
