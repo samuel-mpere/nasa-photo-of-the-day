dccb1604191c7af32e7c2939bfdc7d9229900997
chudnofskyATD <mpereannor@gmail.com>
2019-09-11 11:52:54 +0000

    scaffolding stage 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
-import "./App.css";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+// import Cart from './Cart';
+// import Fruits from './Fruits';
+import './App.css';
+
+const placeHolderApi = 'https://lambda-github-api-server.herokuapp.com/';
+
 
 function App() {
+
+  const [photo, setPhoto] = useState([]);
+
+  //const method to render child components
+
+  useEffect(() => {
+    console.log('faut que ca marche!')
+  });
+
+  useEffect (() => {
+    axios.get(placeHolderApi)
+    .then(response => {
+      //method to render child components.response.data
+    })
+    .catch(error => {
+      'Error!', error
+    });
+  }, []);
+
   return (
     <div className="App">
       <p>
