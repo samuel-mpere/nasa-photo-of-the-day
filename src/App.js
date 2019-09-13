b0420462e77cd9a10d839e47de694b9c78b206f6
chudnofskyATD <mpereannor@gmail.com>
2019-09-13 05:34:29 +0000

    NASA APOD DAY 2 - FINAL PROJECT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from "react"
 import axios from 'axios';
-import './App.css';
-import Photo from './Components/Photo';
-import Details from './Components/Details';
-
+import styled from 'styled-components';
+import {Details, Photo} from './Components';
+// import './App.css';
 
 const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=S2uKImruqWbDh0npoFiO4tmQ3y8WaPlTRymhMSnG';
 
@@ -17,18 +16,33 @@ function App() {
       setComponents(response.data)
     })
     .catch(error => {
-      console.log('error');
+      console.log (error);
     });
   }, []);
 
   const{title, url, date,  explanation} = components;
 
+  
+    
+  const ContainerStyle = styled.div`
+
+  display: flex;
+  padding: 80px;
+  background-image: linear-gradient(#141E30, #243B55);
+  vertical-align: baseline;
+  `
+
+
   return (
     <div className="App">
-      <Photo image={url}/>
-      <div>
-        <Details title={title} date={date} explanation={explanation}/>
-      </div>
+      <ContainerStyle>
+        <div>
+          <Photo image={url}/>
+        </div>
+        <div>
+          <Details title={title} date={date} explanation={explanation}/>
+        </div>
+      </ContainerStyle>
 
       {/* <p>
         Read through the instructions in the README.md file to build your NASA
